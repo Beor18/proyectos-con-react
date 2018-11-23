78ffbfd88beac96275458887efffab80cd6fc857
Beor18 <fer.ragnar19@gmail.com>
2018-11-23 18:59:37 -0300

    se consume la api y se muestra data en tiempo real

--- demo002/src/App.js
+++ demo002/src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import socketIOClient from 'socket.io-client';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://localhost:3000"
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromApi", data => this.setState({response: data}));
+  }
   render() {
+    const { response } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{textAlign: "center"}}>
+        {response
+          ? <p>
+              La data es: {response}
+            </p>
+          : <p>Cargando...</p>
+        }
       </div>
     );
   }
