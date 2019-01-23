ff3db457a0e95d2fa23cc0536247c718dd77350e
Beor18 <fer.ragnar19@gmail.com>
2019-01-23 09:11:53 -0300

    configurando Store

--- demo003/src/index.js
+++ demo003/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import rootReducer from './reducers';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+
+registerServiceWorker();
