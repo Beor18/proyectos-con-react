5f42e33c2bd9ef11a406c4a868fc488957c1ea6e
Beor18 <fer.ragnar19@gmail.com>
2019-01-23 08:27:54 -0300

    agregado componente NuevoPost

--- demo003/src/App.js
+++ demo003/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NuevoPost from './components/NuevoPost';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container" style={{marginTop: '30px'}}>
+        <div className="row">
+          <div className="col-md-6">
+            <NuevoPost />
+          </div>
+          <div className="col-md-6">
+            Todos las Notas
+          </div>
+        </div>
       </div>
     );
   }
--- demo003/src/components/NuevoPost.js
+++ demo003/src/components/NuevoPost.js
@@ -1,5 +1,67 @@
 import React from 'react';
 
 class NuevoPost extends React.Component {
-    
+    state = {
+        titulo: '',
+        body: ''
+    };
+
+    handleInputChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        if (this.state.titulo.trim() && this.state.body.trim()) {
+            console.log(this.state);
+            this.handleReset();
+        }
+    };
+
+    handleReset = () => {
+        this.setState({
+            titulo: '',
+            body: ''
+        });
+    };
+
+    render() {
+        return (
+            <div className="container">
+            <form onSubmit={ this.handleSubmit }>
+            <div className="form-group">
+                <input
+                type="text"
+                placeholder="Titulo"
+                className="form-control"
+                name="title"
+                onChange={ this.handleInputChange }
+                value={ this.state.titulo }
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                cols="19"
+                rows="8"
+                placeholder="Cuerpo"
+                className="form-control"
+                name="body"
+                onChange={ this.handleInputChange }
+                value={ this.state.body }>
+              </textarea>
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary" style={{marginRight: '10px'}}>Agregar Nota</button>
+              <button type="button" className="btn btn-danger" onClick={ this.handleReset }>
+                Resetear
+              </button>
+            </div>
+          </form>
+        </div>
+        );
+    }
 }
+
+export default NuevoPost;
