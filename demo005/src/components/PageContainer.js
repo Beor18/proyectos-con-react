ee166cebfd513cebf072383950b616bc3bff002b
Beor18 <fer.ragnar19@gmail.com>
2019-01-28 21:21:16 -0300

    estilos y manejo de comando por teclado

--- demo005/src/components/PageContainer.js
+++ demo005/src/components/PageContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Editor, EditorState } from "draft-js";
+import { Editor, EditorState, RichUtils } from "draft-js";
 
 class PageContainer extends React.Component {
     constructor(props) {
@@ -15,12 +15,23 @@ class PageContainer extends React.Component {
         });
     };
 
+    // Manejo de comando de teclado para poner un texto en negrita o subrayado
+    handleKeyCommand = (command) => {
+        const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+        if (newState) {
+            this.onChange(newState);
+            return 'handled';
+        }
+        return 'no handled';
+    }
+
     render() {
         return (
             <div className="editorContainer">
                 <div className="editors">
                     <Editor
                         editorState={this.state.editorState}
+                        handleKeyCommand={this.handleKeyCommand}
                         onChange={this.onChange}
                     />
                 </div>
