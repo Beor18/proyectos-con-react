78af4721933ef592d151c6f8965c08b1c1aec1d3
Beor18 <fer.ragnar19@gmail.com>
2019-02-08 23:06:35 -0300

    agregado material ui

--- demo005/src/components/listaHoteles/HotelListaItem.js
+++ demo005/src/components/listaHoteles/HotelListaItem.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as Actions from '../../actions';
 import moment from 'moment';
 require('moment/locale/es');
+
 
 class HotelListaItem extends React.Component {
   constructor(props){
@@ -26,13 +34,35 @@ class HotelListaItem extends React.Component {
     this.setState({
       actualizarTiempo : moment(this.props.lista.created_at).locale('es').fromNow()})
   }
-    
+  
+  mediaCard() {
+    return (
+        <Card key={this.props.lista._id}>
+          <CardActionArea>
+            <CardMedia
+              image="http://forbes.es/app/uploads/2017/05/2017626105313_3.jpg"
+              title="Contemplative Reptile"
+              style={{height: 140}}
+            />
+            <CardContent>
+              <Typography className="titulo-card" gutterBottom variant="h5" component="h2">
+                {this.props.lista.title}
+              </Typography>
+              <Typography component="p">
+                {/* {this.props.lista.content} */}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <p>{this.state.actualizarTiempo}</p>
+          </CardActions>
+        </Card>
+    );
+  }
   render() {
     return(
-      <div key={this.props.lista._id} className="note-list-item" onClick={this.props.selectNote}>
-        <p>{this.props.lista.title}</p>
-        <p>{this.props.lista.content}</p>
-        <p>{this.state.actualizarTiempo}</p>
+      <div onClick={this.props.selectNote}>
+        {this.mediaCard()}
       </div>
 
     )
