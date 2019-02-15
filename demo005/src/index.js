bb5f3cfb9f6a0de67981036203a68f8106cc963e
Beor18 <fer.ragnar19@gmail.com>
2019-02-15 11:00:33 -0300

    devtools

--- demo005/src/index.js
+++ demo005/src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
 
+const composeEnhancers =
+typeof window === 'object' &&
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+  }) : compose;
+
+const enhancer = composeEnhancers(
+applyMiddleware(thunk),
+);
+
 function configureStore() {
-    return createStore(rootReducer, applyMiddleware(thunk));
+    return createStore(rootReducer, enhancer);
 }
    
 const store = configureStore();
