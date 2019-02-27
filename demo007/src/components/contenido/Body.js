6b420e8211bb30ababad2405d1f72e62699baffe
Beor18 <fer.ragnar19@gmail.com>
2019-02-26 22:09:29 -0300

    agregado componentes

---
+++ demo007/src/components/contenido/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PriceCard from './card/PrecioCard';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import Widgets from 'fusioncharts/fusioncharts.widgets';
+import ReactFC from 'react-fusioncharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+class Body extends React.Component {
+    componentDidMount() {
+
+    }
+}
+
+export default Body;
