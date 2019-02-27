82f83f74cc75e552415ede334d877acd2b3d1559
Beor18 <fer.ragnar19@gmail.com>
2019-02-27 09:14:01 -0300

    agregado component header

---
+++ demo007/src/components/encabezado/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Header = props => {
+    const {branding} = props;
+    return (
+        <nav className="navbar mb-3 pt-3 pb-3 py-0  text-sm-center text-md-left">
+            <div className="container">
+            <a href="/" className="navbar-brand">
+                {branding}
+            </a>
+            <ul className="navbar-nav mr-auto-right" alt="fireSpot">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <img src={"fclogo.svg"} width="145" alt="" />
+                </li>
+                </ul>
+            </ul>
+            </div>
+        </nav>
+    );
+};
+
+Header.defaultProps = {
+    branding: "Un Texto encabezado"
+};
+
+export default Header;
