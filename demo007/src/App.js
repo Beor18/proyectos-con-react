2daf8f8430ffdbed2ee8922fae44b005365aa564
Beor18 <fer.ragnar19@gmail.com>
2019-02-27 15:56:20 -0300

    estilos

--- demo007/src/App.js
+++ demo007/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/encabezado/Header';
-
+import Body from './components/contenido/Body'
+import './App.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Header />
+        <div className="App container">
+          <Body />
+        </div>
       </div>
     );
   }
