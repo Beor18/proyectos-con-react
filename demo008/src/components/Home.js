f2d597179c538ad40498b3e0265b54473e13a7aa
Beor18 <fer.ragnar19@gmail.com>
2019-03-15 21:12:12 -0300

    agregado componentes

---
+++ demo008/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Collapsible from 'react-collapsible';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Collapsible>
+            Prueba Accordion
+        </Collapsible>
+      </div>
+    );
+  }
+}
+
+export default Home;
