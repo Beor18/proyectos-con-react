2be7672e74c328a25370833f0012c0185eedc5d3
Beor18 <fer.ragnar19@gmail.com>
2019-03-25 20:33:55 -0300

    modificado

--- demo009/src/App.js
+++ demo009/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-         
+          <h2>Buscador de Peliculas con Hooks!</h2>
         </header>
+
+        <Home />
       </div>
     );
   }
