20c428635b2a8d6caada849908071b046774c530
Beor18 <fer.ragnar19@gmail.com>
2019-03-25 20:34:44 -0300

    agregado componente buscar

---
+++ demo009/src/components/Buscar.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Buscar = ({ search }) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchInputChanges = e => {
+    setSearchValue(e.target.value);
+  };
+
+  const resetInputField = () => {
+    setSearchValue("");
+  };
+
+  const callSearchFunction = e => {
+    e.preventDefault();
+    search(searchValue);
+    resetInputField();
+  };
+
+  return (
+    <form className="search">
+      <input
+        value={searchValue}
+        onChange={handleSearchInputChanges}
+        type="text"
+      />
+      <input onClick={callSearchFunction} type="submit" value="BUSCAR" />
+    </form>
+  );
+};
+
+export default Buscar;
