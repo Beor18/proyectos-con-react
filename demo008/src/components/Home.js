b0cf5c6d950a49876fc889c7a631982c6edbb84a
Beor18 <fer.ragnar19@gmail.com>
2019-04-01 21:39:35 -0300

    agregado nuevo componente

--- demo008/src/components/Home.js
+++ demo008/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Colapsable from '../components/Colapsable';
 import Ejemplo from '../components/Hooks';
+import Consumir from '../components/Consumir'
 
 function Home() {
     return (
       <div>
         <Colapsable />
         <Ejemplo />
+        <Consumir />
       </div>
     );
 }
