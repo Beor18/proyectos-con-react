7c9f4c27a0e6a3e0c2a7ea06c236b3654c20a7cb
Beor18 <fer.ragnar19@gmail.com>
2019-04-17 20:53:48 -0300

    algunas pruebas mas con bulma

--- demo010/src/components/columnaIndividual.js
+++ demo010/src/components/columnaIndividual.js
@@ -1,14 +1,46 @@
 import React, {Component} from "react";
-import { Notification, Container } from "rbx";
+import { Content, Title, Container, Hero } from "rbx";
 import "rbx/index.css";
 
 class ColumnaIndividual extends Component {
     render() {
         return (
             <Container>
-                 <Notification color="primary" textAlign="centered">
-                    Columna
-                 </Notification>
+                <Hero color="primary">
+                    <Hero.Body>
+                        <Container>
+                            <Title>COLUMNA</Title>
+                            <Title as="h2" subtitle>
+                                Esto es una prueba con Bulma UI
+                            </Title>
+                        </Container>
+                    </Hero.Body>
+                </Hero>
+                 <Content>
+                    <Title>Hola Mundo!</Title>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan,
+                        metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum
+                        justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio,
+                        sollicitudin vel erat vel, interdum mattis neque.
+                    </p>
+                    <Title as="h2">Segundo título</Title>
+                    <p>
+                        Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong>{' '}
+                        blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id.
+                        Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis
+                        sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque
+                        nisl.
+                    </p>
+                    <ul>
+                        <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
+                        <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
+                        <li>
+                            Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.
+                        </li>
+                        <li>Ut non enim metus.</li>
+                    </ul>
+               </Content>
             </Container>
         )
   }
