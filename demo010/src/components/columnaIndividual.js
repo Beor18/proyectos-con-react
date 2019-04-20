9f6cce9d8ca2c815022e782b1616e4f8a5e08d5e
Beor18 <fer.ragnar19@gmail.com>
2019-04-19 21:09:18 -0300

    cambio en componente

--- demo010/src/components/columnaIndividual.js
+++ demo010/src/components/columnaIndividual.js
@@ -5,42 +5,46 @@ import "rbx/index.css";
 class ColumnaIndividual extends Component {
     render() {
         return (
-            <Container>
-                <Hero color="primary">
-                    <Hero.Body>
-                        <Container>
-                            <Title>COLUMNA</Title>
-                            <Title as="h2" subtitle>
-                                Algo
-                            </Title>
-                        </Container>
-                    </Hero.Body>
-                </Hero>
-                 <Content>
-                    <Title>Hola Mundo!</Title>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan,
-                        metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum
-                        justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio,
-                        sollicitudin vel erat vel, interdum mattis neque.
-                    </p>
-                    <Title as="h2">Segundo título</Title>
-                    <p>
-                        Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong>{' '}
-                        blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id.
-                        Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis
-                        sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque
-                        nisl.
-                    </p>
-                    <ul>
-                        <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
-                        <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
-                        <li>
-                            Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.
-                        </li>
-                        <li>Ut non enim metus.</li>
-                    </ul>
-               </Content>
+            <Container fluid style={{marginLeft: '0px', marginRight: '0px'}}>
+                <Content>
+                    <Hero color="primary">
+                        <Hero.Body>
+                            <Container>
+                                <Title>COLUMNA INDIVIDUAL</Title>
+                                <Title as="h3" subtitle>
+                                    Esto es un subtítulo
+                                </Title>
+                            </Container>
+                        </Hero.Body>
+                    </Hero>
+                </Content>
+                <Container>
+                    <Content>
+                        <Title>Hola Mundo!</Title>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan,
+                            metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum
+                            justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio,
+                            sollicitudin vel erat vel, interdum mattis neque.
+                        </p>
+                        <Title as="h2">Segundo título</Title>
+                        <p>
+                            Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong>{' '}
+                            blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id.
+                            Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis
+                            sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque
+                            nisl.
+                        </p>
+                        <ul>
+                            <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
+                            <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
+                            <li>
+                                Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.
+                            </li>
+                            <li>Ut non enim metus.</li>
+                        </ul>
+                </Content>
+               </Container>
             </Container>
         )
   }
