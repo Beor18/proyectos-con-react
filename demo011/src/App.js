c7010099b9f2f734c7d50cc2bae20a51011154bc
Beor18 <fer.ragnar19@gmail.com>
2019-04-22 21:31:38 -0300

    cambio en app

--- demo011/src/App.js
+++ demo011/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Autocompletar from './components/Autocompletar';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Autocompletar />
+      </div>
+    );
+  }
 }
 
 export default App;
