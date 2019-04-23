ce967f73249b5dae265def6141af29b5818c0ec6
Beor18 <fer.ragnar19@gmail.com>
2019-04-22 22:00:34 -0300

    agregado lista de sugerencias

--- demo011/src/App.js
+++ demo011/src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Autocompletar />
+        <Autocompletar 
+          sugerencias={["Mama", "Papa", "Nodejs", "Azul", "Amarillo", "react"]}
+        />
       </div>
     );
   }
